8f802b7b972be7df69a1cb98c61b3413f2d7d6eb
Ryan Adhitama Putra <ryanadhitama2@gmail.com>
2021-03-22 15:19:51 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+seven_day = soup.find(id="seven-day-forecast-body")
+period_tags = seven_day.select(".tombstone-container .period-name")
+
+periods = [pt.get_text() for pt in period_tags]
+short_desc = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+
+weather = pd.DataFrame({
+    "period" : periods,
+    "short_desc": short_desc,
+    "temp": temps,
+    "desc":descs
+})
+
+print(weather)
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Weather Scrapping
+
+This is my simple python weather scrapping from [7 day forecast](https://forecast.weather.gov/MapClick.php?lat=34.0536&lon=-118.2455#.YFhAmEgzZQI) website.
+
+## How to use:
+Install requirement :
+```sh 
+pip install -r requirements.txt 
+```
+
+Run application :
+```sh 
+python main.py
+```
